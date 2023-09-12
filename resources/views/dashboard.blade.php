d93f1a0a56bafb5a05ae951d7c95dcc7265fc6d9
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2023-09-12 16:50:27 -0600

    Modificación de plantilla jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,22 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Jetstream
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            @php
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum eum laudantium sequi sed veniam. Voluptatem praesentium fugit consequuntur officiis ad saepe excepturi, quidem alias aperiam beatae perferendis eaque aut nostrum.</p>
+                <p>Necessitatibus quis voluptate quasi veritatis cum autem eligendi, deleniti placeat sequi inventore fugit, odit, officiis illo alias! Mollitia distinctio illum eaque sit quaerat voluptate nulla nobis, eius ipsum sapiente aperiam?</p>
+                <p>Minima similique odio, consequuntur, quibusdam facilis nulla necessitatibus veniam, voluptates error rem ex cumque at. Non, excepturi maiores alias porro nobis ex reiciendis aut ipsam eaque tempora, dolores, quam ad.</p>
+                <p>Molestiae nisi mollitia deserunt aut nemo. Non eveniet ipsum esse, culpa illo libero eaque deserunt iste officiis repudiandae sunt harum labore unde sequi tempore sint perspiciatis, et eum impedit possimus!</p>
+                <p>Nemo minus eaque, sint, commodi nesciunt saepe minima amet nostrum, rerum quos dolores architecto consectetur iusto quas hic consequatur? Quia, voluptate libero neque eum quas ratione nihil exercitationem saepe voluptas!</p>
+            </div>
+
+            {{-- @php
                 $type = 'danger';
             @endphp
             
@@ -19,7 +27,7 @@
             <p>Este es un texto de prueba</p>
         </x-alert2>
 
-        <p>Hola Mundo</p>
+        <p>Hola Mundo</p> --}}
 
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware([
     'auth:sanctum',
@@ -25,4 +25,12 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::get('prueba', function(){
+    return "Has accedido correctamente a esta ruta";
+})->middleware(['auth:sanctum', 'age']);
+
+Route::get('no-autorizado', function(){
+    return "Usted no es mayor de edad";
 });
